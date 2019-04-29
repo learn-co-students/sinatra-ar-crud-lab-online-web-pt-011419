edc0e62232c41c35d1b9573d47f340d7ca9547c2
dylanclements <dclements@protonmail.com>
2019-04-29 10:40:32 -0400

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,19 @@
 #Placeholder for a model
+require 'pry'
 
 class Article < ActiveRecord::Base
+    # attr_accessor :title, :content
+    # @@articles = []
+
+    # def initialize(params)
+        
+    #     @title = params[:title]
+    #     @content = params[:content]
+    #     @@articles << self
+    # end
+
+    # def self.all
+    #     @@articles
+    # end
+
 end
