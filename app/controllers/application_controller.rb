bc4d7104ba0963bbbdf436d21eebf160e1abb487
Nissan Holzer <nissan.holzer@gmail.com>
2019-05-10 00:41:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < Sinatra::Base
   get '/' do
   end
   
+  get '/articles' do #index shows all of the objects
+    @articles = Article.all
+  end
+  
   get '/articles/new' do
     erb :new
   end
